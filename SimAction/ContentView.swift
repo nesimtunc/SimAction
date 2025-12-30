7143d94a1fb543e0b07ec4a83ea4b64eee31aabf
Nesim Tunc <nesimtunc@gmail.com>
2025-12-30 13:50:49 +0900

    Create MacOS project

---
+++ SimAction/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimAction
+//
+//  Created by Nesim Tunç on 2025/12/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimAction/SimActionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimActionApp.swift
+//  SimAction
+//
+//  Created by Nesim Tunç on 2025/12/30.
+//
+
+import SwiftUI
+
+@main
+struct SimActionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
